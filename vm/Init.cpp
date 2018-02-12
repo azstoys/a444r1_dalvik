14c637c771f7b6e04b2f47ba89852942a12c7db4
salvatore posborne <sal.posborne@gmail.com>
2018-02-11 16:46:47 -0800

    add a code snippet to print stack trace when dvm starts up, to be tested on nexus5

--- vm/Init.cpp
+++ vm/Init.cpp
@@ -18,6 +18,10 @@
  * Dalvik initialization, shutdown, and command-line argument processing.
  */
 #define __STDC_LIMIT_MACROS
+// Added By WZ
+#include <utils/CallStack.h>
+#include <cutils/properties.h>
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <signal.h>
@@ -1382,6 +1386,17 @@ private:
 std::string dvmStartup(int argc, const char* const argv[],
         bool ignoreUnrecognized, JNIEnv* pEnv)
 {
+ 
+#ifndef ARCH_IA32
+    char propBuf[PROPERTY_VALUE_MAX];
+    property_get("dvmbtrace.Init.dvmStartup", propBuf, "");
+    if (strcmp(propBuf, "true") == 0) {
+        android::CallStack stack;
+        stack.update();
+        stack.log("azstoys@gmail.com", ANDROID_LOG_DEBUG, "DEAZ");
+    }
+#endif
+
     ScopedShutdown scopedShutdown;
 
     assert(gDvm.initializing);
